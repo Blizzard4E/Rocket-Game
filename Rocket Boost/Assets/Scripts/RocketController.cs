9c996c9854b9875e89b2991e48c9dcfaf06af3cb
Blizzard4E <phenggamingone@gmail.com>
2020-12-26 19:00:13 +0700

    Added Rocket to Unity Game
    -Functional Rocket Controls

---
+++ Rocket Boost/Assets/Scripts/RocketController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public float thrustForce = 10f;
+    public float rotateDegree = 25f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = Vector3.zero;
+        rb.inertiaTensorRotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -rotateDegree * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotateDegree * Time.deltaTime);
+        }
+    }
+}
